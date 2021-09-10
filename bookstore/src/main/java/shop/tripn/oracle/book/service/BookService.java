402ad47a86498598f53b69011b8e85323d297e56
litecj <litecj702@gmail.com>
2021-09-10 10:50:18 +0900

    hum

--- bookstore/src/main/java/shop/tripn/oracle/book/service/BookService.java
+++ bookstore/src/main/java/shop/tripn/oracle/book/service/BookService.java
@@ -1,8 +1,11 @@
 package shop.tripn.oracle.book.service;
 
+import java.util.List;
 import org.springframework.stereotype.Component;
+import shop.tripn.oracle.book.domain.BookDto;
 
 @Component 
 public interface BookService {
+	public List<BookDto> findAll();
 
 }
--- bookstore/src/main/java/shop/tripn/oracle/book/service/BookServiceImpl.java
+++ bookstore/src/main/java/shop/tripn/oracle/book/service/BookServiceImpl.java
@@ -1,8 +1,16 @@
 package shop.tripn.oracle.book.service;
 
+import java.util.List;
 import org.springframework.stereotype.Service;
+import shop.tripn.oracle.book.domain.BookDto;
 
 @Service
 public class BookServiceImpl implements BookService {
+
+	@Override
+	public List<BookDto> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
